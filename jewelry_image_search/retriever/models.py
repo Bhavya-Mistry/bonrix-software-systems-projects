72565aa0dfbec306c9368080ebc90b3c6962a70c
Bhavya-Mistry <mistrybhavya9@gmail.com>
2025-08-21 15:50:59 +0530

    Update models.py

--- jewelry_image_search/retriever/models.py
+++ jewelry_image_search/retriever/models.py
@@ -5,13 +5,13 @@ import open_clip
 # Define available models here
 CLIP_MODELS = {
     'ViT-B/32': 'laion2b_s34b_b79k',  # default model
-    'ViT-L/14': 'laion2b_s32b_b82k',
+    'ViT-L/14': 'laion2b_s34b_b79k',
     'RN50': 'openai/clip-vit-base-patch32',
     'RN101': 'openai/clip-vit-large-patch16'
 }
 
 # Set your desired model from the available ones
-SELECTED_MODEL = 'ViT-L/14'   # Change this to any model from the list
+SELECTED_MODEL = 'ViT-B/32'  # Change this to any model from the list
 
 # Model setup
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
